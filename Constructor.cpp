1054f7948aa0369d472a0d827f1421caaebc2241
Ayush Verma <averma95484@gmail.com>
2025-08-26 12:35:51 +0530

    Update and rename constructor.cpp to Constructor.cpp

--- Constructor.cpp
+++ Constructor.cpp
@@ -6,17 +6,15 @@ class Function
     public :
     int id;
     char name[20];
-    char mobile[20];
     Function()
     {
         cout<<"ID : ";
         cin>>id;
         strcpy(name,"AyushVerma");
-        strcpy(mobile,"9411835360");
     }
     void print()
     {
-        cout<<id<<endl<<name<<endl<<mobile;
+        cout<<id<<endl<<name;
     }
 };
 int main()
